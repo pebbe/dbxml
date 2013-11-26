ce96cf20937863596ee123c0936eca2cbca26da2
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2013-11-26 17:56:57 +0100

    First commit.

---
+++ c_dbxml.cc
@@ -0,0 +1,253 @@
+#include "c_dbxml.h"
+#include <dbxml/DbXml.hpp>
+#include <string>
+
+#define ALIAS "c_dbxml"
+
+extern "C" {
+
+    struct c_dbxml_t {
+	DbXml::XmlManager manager;
+	DbXml::XmlUpdateContext context;
+	DbXml::XmlContainer container;
+	bool error;
+	std::string errstring;
+	std::string result;
+    };
+
+    struct c_dbxml_docs_t {
+	DbXml::XmlDocument doc;
+	DbXml::XmlResults it;
+	DbXml::XmlQueryContext context;
+	bool more;
+	std::string name;
+	std::string content;
+    };
+
+    c_dbxml c_dbxml_open(char const *filename)
+    {
+	c_dbxml db;
+
+	db = new c_dbxml_t;
+
+	db->error = false;
+	db->errstring = "";
+
+	try {
+	    db->context = db->manager.createUpdateContext();
+	    db->container = db->manager.existsContainer(filename) ?
+		db->manager.openContainer(filename) :
+		db->manager.createContainer(filename);
+	    if (!db->container.addAlias(ALIAS)) {
+		db->errstring = "Unable to add alias \"" ALIAS "\"";
+		db->error = true;
+	    }
+	} catch (DbXml::XmlException &xe) {
+	    db->errstring = xe.what();
+	    db->error = true;
+	}
+
+	return db;
+    }
+
+    void c_dbxml_free(c_dbxml db)
+    {
+	delete db;
+    }
+
+    int c_dbxml_error(c_dbxml db)
+    {
+	return db->error ? 1 : 0;
+    }
+
+    char const *c_dbxml_errstring(c_dbxml db)
+    {
+	return db->errstring.c_str();
+    }
+
+    int c_dbxml_put_file(c_dbxml db, char const * filename, int replace)
+    {
+	db->errstring = "";
+	db->error = false;
+
+	if (replace) {
+	    try {
+		db->container.deleteDocument(filename, db->context);
+	    } catch (DbXml::XmlException &xe) {
+		;
+	    }
+	}
+        try {
+            DbXml::XmlInputStream *is = db->manager.createLocalFileInputStream(filename);
+            db->container.putDocument(filename, is, db->context);
+        } catch (DbXml::XmlException &xe) {
+	    db->error = true;
+	    db->errstring = xe.what();
+	    return 0;
+        }
+
+	return 1;
+    }
+
+    /* replace if replace != 0
+     * return 0 bij fout
+     */
+    int c_dbxml_put_xml(c_dbxml db, char const *name, char const *data, int replace)
+    {
+	db->errstring = "";
+	db->error = false;
+
+	if (replace) {
+	    try {
+		db->container.deleteDocument(name, db->context);
+	    } catch (DbXml::XmlException &xe) {
+		;
+	    }
+	}
+
+        try {
+            db->container.putDocument(name, data, db->context);
+        } catch (DbXml::XmlException &xe) {
+	    db->error = true;
+	    db->errstring = xe.what();
+	    return 0;
+        }
+	return 1;
+    }
+
+    /* replace if replace != 0
+     * return 0 bij fout
+     */
+    int c_dbxml_merge(c_dbxml db, char const * dbxmlfile, int replace) {
+	db->errstring = "";
+	db->error = false;
+
+	DbXml::XmlContainer input = db->manager.openContainer(dbxmlfile);
+	DbXml::XmlDocument doc;
+	DbXml::XmlResults it = input.getAllDocuments(DbXml::DBXML_LAZY_DOCS);
+	while (it.next(doc)) {
+	    if (replace) {
+		try {
+		    db->container.deleteDocument(doc.getName(), db->context);
+		} catch (DbXml::XmlException &xe) {
+		    ;
+		}
+	    }
+	    try {
+		db->container.putDocument(doc, db->context);
+	    } catch (DbXml::XmlException &xe) {
+		db->error = true;
+		db->errstring = xe.what();
+		return 0;
+	    }
+	}
+	return 1;
+    }
+
+    int c_dbxml_remove(c_dbxml db, char const * filename)
+    {
+	db->errstring = "";
+	db->error = false;
+
+	try {
+	    db->container.deleteDocument(filename, db->context);
+        } catch (DbXml::XmlException &xe) {
+	    db->error = true;
+	    db->errstring = xe.what();
+	    return 0;
+	}
+	return 1;
+    }
+
+
+    char const * c_dbxml_get(c_dbxml db, char const * name)
+    {
+	db->errstring = "";
+	db->error = false;
+
+	try {
+	    DbXml::XmlDocument doc = db->container.getDocument(name);
+	    doc.getContent(db->result);
+	    return db->result.c_str();
+	} catch (DbXml::XmlException &xe) {
+	    db->errstring = xe.what();
+	    db->error = true;
+	    return db->errstring.c_str();
+	}
+    }
+
+    unsigned long long c_dbxml_size(c_dbxml db)
+    {
+	return (unsigned long long) db->container.getNumDocuments();
+    }
+
+    c_dbxml_docs c_dbxml_get_all(c_dbxml db)
+    {
+	db->errstring = "";
+	db->error = false;
+
+	c_dbxml_docs docs;
+	docs = new c_dbxml_docs_t;
+	docs->it = db->container.getAllDocuments(DbXml::DBXML_LAZY_DOCS);
+	docs->more = true;
+	return docs;
+    }
+
+    c_dbxml_docs c_dbxml_get_query(c_dbxml db, char const *query)
+    {
+	db->errstring = "";
+	db->error = false;
+
+	c_dbxml_docs docs;
+	docs = new c_dbxml_docs_t;
+	docs->more = true;
+	try {
+
+	    docs->context = db->manager.createQueryContext(DbXml::XmlQueryContext::LiveValues, DbXml::XmlQueryContext::Lazy);
+	    docs->context.setDefaultCollection(ALIAS);
+	    docs->it = db->manager.query(std::string("collection('" ALIAS "')") + query,
+					 docs->context,
+					 DbXml::DBXML_LAZY_DOCS | DbXml::DBXML_WELL_FORMED_ONLY
+					 );
+
+	} catch (DbXml::XmlException const &xe) {
+	    docs->more = false;
+	    db->error = true;
+	    db->errstring = xe.what();
+	}
+
+	return docs;
+    }
+
+    int c_dbxml_docs_next(c_dbxml_docs docs)
+    {
+	if (docs->more) {
+	    docs->more = docs->it.next(docs->doc);
+	    docs->name.clear();
+	    docs->content.clear();
+	}
+	return docs->more ? 1 : 0;
+    }
+
+    char const * c_dbxml_docs_name(c_dbxml_docs docs)
+    {
+	if (docs->more && ! docs->name.size())
+	    docs->name = docs->doc.getName();
+
+	return docs->name.c_str();
+    }
+
+    char const * c_dbxml_docs_content(c_dbxml_docs docs)
+    {
+	if (docs->more && ! docs->content.size())
+	    docs->doc.getContent(docs->content);
+
+	return docs->content.c_str();
+    }
+
+    void c_dbxml_docs_free(c_dbxml_docs docs)
+    {
+	delete docs;
+    }
+
+}
---
+++ dbxml.go
@@ -0,0 +1,293 @@
+/*
+A basic Go interface to Oracle Berkeley DB XML.
+
+http://www.oracle.com/us/products/database/berkeley-db/xml/index.html
+*/
+package dbxml
+
+//. Imports
+
+/*
+#cgo LDFLAGS: -ldbxml
+#include <stdlib.h>
+#include "c_dbxml.h"
+*/
+import "C"
+
+import (
+	"errors"
+	"runtime"
+	"sync"
+	"unsafe"
+)
+
+//. Types
+
+// A database connection.
+type Db struct {
+	opened bool
+	db     C.c_dbxml
+	lock   sync.Mutex
+}
+
+// An iterator over xml documents in the database.
+type Docs struct {
+	opened bool
+	docs   C.c_dbxml_docs
+	lock   sync.Mutex
+}
+
+//. Variables
+
+var (
+	errclosed = errors.New("Database is closed")
+)
+
+//. Open & Close
+
+// Open a database.
+//
+// Call db.Close() to ensure all write operations to the database are finished, before terminating the program.
+func Open(filename string) (*Db, error) {
+	db := &Db{}
+	cs := C.CString(filename)
+	defer C.free(unsafe.Pointer(cs))
+	db.db = C.c_dbxml_open(cs)
+	if C.c_dbxml_error(db.db) != 0 {
+		err := errors.New(C.GoString(C.c_dbxml_errstring(db.db)))
+		C.c_dbxml_free(db.db)
+		return db, err
+	}
+	db.opened = true
+	runtime.SetFinalizer(db, (*Db).Close)
+	return db, nil
+}
+
+// Close the database.
+//
+// This flushes all write operations to the database.
+func (db *Db) Close() {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+	if db.opened {
+		C.c_dbxml_free(db.db)
+		db.opened = false
+	}
+}
+
+//. Write
+
+// Put an xml file from disc into the database.
+func (db *Db) PutFile(filename string, replace bool) error {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	if !db.opened {
+		return errclosed
+	}
+
+	cs := C.CString(filename)
+	defer C.free(unsafe.Pointer(cs))
+	repl := C.int(0)
+	if replace {
+		repl = 1
+	}
+	if C.c_dbxml_put_file(db.db, cs, repl) == 0 {
+		return errors.New(C.GoString(C.c_dbxml_errstring(db.db)))
+	}
+	return nil
+}
+
+// Put an xml document from memory into the database.
+func (db *Db) PutXml(name string, data string, replace bool) error {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	if !db.opened {
+		return errclosed
+	}
+
+	csname := C.CString(name)
+	defer C.free(unsafe.Pointer(csname))
+	csdata := C.CString(data)
+	defer C.free(unsafe.Pointer(csdata))
+	repl := C.int(0)
+	if replace {
+		repl = 1
+	}
+	if C.c_dbxml_put_xml(db.db, csname, csdata, repl) == 0 {
+		return errors.New(C.GoString(C.c_dbxml_errstring(db.db)))
+	}
+	return nil
+}
+
+// Merge a database from disc into this database.
+func (db *Db) Merge(filename string, replace bool) error {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	if !db.opened {
+		return errclosed
+	}
+
+	cs := C.CString(filename)
+	defer C.free(unsafe.Pointer(cs))
+	repl := C.int(0)
+	if replace {
+		repl = 1
+	}
+	if C.c_dbxml_merge(db.db, cs, repl) == 0 {
+		return errors.New(C.GoString(C.c_dbxml_errstring(db.db)))
+	}
+	return nil
+}
+
+// Remove an xml document from the database.
+func (db *Db) Remove(name string) error {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	if !db.opened {
+		return errclosed
+	}
+
+	cs := C.CString(name)
+	defer C.free(unsafe.Pointer(cs))
+	if C.c_dbxml_remove(db.db, cs) == 0 {
+		return errors.New(C.GoString(C.c_dbxml_errstring(db.db)))
+	}
+	return nil
+}
+
+//. Read
+
+// Get an xml document by name from the database.
+func (db *Db) Get(name string) (string, error) {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	if !db.opened {
+		return "", errclosed
+	}
+
+	cs := C.CString(name)
+	defer C.free(unsafe.Pointer(cs))
+	s := C.GoString(C.c_dbxml_get(db.db, cs))
+	if C.c_dbxml_error(db.db) != 0 {
+		return "", errors.New(s)
+	}
+	return s, nil
+}
+
+// Get the number of xml documents in the database.
+func (db *Db) Size() (uint64, error) {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	if !db.opened {
+		return 0, errclosed
+	}
+	return uint64(C.c_dbxml_size(db.db)), nil
+}
+
+// Get all xml documents from the database.
+//
+// Example:
+//
+//      docs, err := db.All()
+//      if err != nil {
+//          fmt.Println(err)
+//      } else {
+//          for docs.Next() {
+//              fmt.Println(docs.Name(), docs.Content())
+//          }
+//      }
+func (db *Db) All() (*Docs, error) {
+	docs := &Docs{}
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	if !db.opened {
+		return docs, errclosed
+	}
+	docs.docs = C.c_dbxml_get_all(db.db)
+	runtime.SetFinalizer(docs, (*Docs).Close)
+	docs.opened = true
+	return docs, nil
+}
+
+// Get all xml documents that match the XPATH query from the database.
+//
+// Example:
+//
+//      docs, err := db.Query(xpath_query)
+//      if err != nil {
+//          fmt.Println(err)
+//      } else {
+//          for docs.Next() {
+//              fmt.Println(docs.Name(), docs.Content())
+//          }
+//      }
+func (db *Db) Query(query string) (*Docs, error) {
+	docs := &Docs{}
+	db.lock.Lock()
+	defer db.lock.Unlock()
+
+	if !db.opened {
+		return docs, errclosed
+	}
+	cs := C.CString(query)
+	defer C.free(unsafe.Pointer(cs))
+	docs.docs = C.c_dbxml_get_query(db.db, cs)
+	if C.c_dbxml_error(db.db) != 0 {
+		err := errors.New(C.GoString(C.c_dbxml_errstring(db.db)))
+		return docs, err
+	}
+	runtime.SetFinalizer(docs, (*Docs).Close)
+	docs.opened = true
+	return docs, nil
+}
+
+// Iterate to the next xml document in the list, that was returned by db.All() or db.Query(query).
+func (docs *Docs) Next() bool {
+	docs.lock.Lock()
+	defer docs.lock.Unlock()
+	if !docs.opened {
+		return false
+	}
+	if C.c_dbxml_docs_next(docs.docs) == 0 {
+		return false
+	}
+	return true
+}
+
+// Get name of current xml document after call to docs.Next().
+func (docs *Docs) Name() string {
+	docs.lock.Lock()
+	defer docs.lock.Unlock()
+	if !docs.opened {
+		return ""
+	}
+	return C.GoString(C.c_dbxml_docs_name(docs.docs))
+}
+
+// Get content of current xml document after call to docs.Next().
+func (docs *Docs) Content() string {
+	docs.lock.Lock()
+	defer docs.lock.Unlock()
+	if !docs.opened {
+		return ""
+	}
+	return C.GoString(C.c_dbxml_docs_content(docs.docs))
+}
+
+// Close iterator over xml documents in the database, that was returned by db.All() or db.Query(query).
+func (docs *Docs) Close() {
+	docs.lock.Lock()
+	defer docs.lock.Unlock()
+	if docs.opened {
+		C.c_dbxml_docs_free(docs.docs)
+		docs.opened = false
+	}
+
+}
